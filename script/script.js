a35dabb60d7b016207afb9fa38dd28680c0d1355
afablelorenz <afablelorenz@gmail.com>
2024-05-07 22:50:46 +0200

    created script

---
+++ script/script.js
@@ -0,0 +1,6 @@
+alert('Password generator!')
+const nome= prompt('Write your Name')
+const cognome = prompt('Write your Surname')
+const color= prompt('Write your favorite color!')
+
+alert('Your password is '+nome+cognome+color+'!')
